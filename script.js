658ddfc30135e42ae3ed0e4c3605738ccf150c77
nick wuerz <nawuerz@gmail.com>
2020-02-01 06:42:13 -0600

    added on click function

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 var searchDiv = $('.searchDiv');
 var searchBtn = $('.searchBtn');
-var cityQuery = $('#cityQuery').value;
+var cityQuery = $('#cityQuery');
 var currentCityDiv = $('.currentCityDiv');
 var forecastDiv = $('.forecastDiv');
 
-var queryURL = "api.openweathermap.org/data/2.5/weather?q=" + cityQuery + "60d05752da37a46049341d1d3af701da";
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + 'dallas' + "&appId=60d05752da37a46049341d1d3af701da";
  
 $.ajax({
   url: queryURL,
   method: "GET"
 }).then(function(response) {
-    
+    searchBtn.on("click", function () {
+      console.log(response);
+      console.log(cityQuery.val());
+    })
 });
