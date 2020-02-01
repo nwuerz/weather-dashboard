c08cd64a7d434511afe23e47426e54a3a85aad09
nick wuerz <nawuerz@gmail.com>
2020-02-01 15:08:18 -0600

    corrected jquery syntax to obtain city for api search

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
+$(document).ready(function () { 
+
 var searchDiv = $('.searchDiv');
 var searchBtn = $('.searchBtn');
-var cityQuery = $('#cityQuery');
+var cityQuery = $('#cityQuery'); 
 var currentCityDiv = $('.currentCityDiv');
 var forecastDiv = $('.forecastDiv');
 
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + 'dallas' + "&appId=60d05752da37a46049341d1d3af701da";
- 
-$.ajax({
-  url: queryURL,
-  method: "GET"
-}).then(function(response) {
-    searchBtn.on("click", function () {
-      console.log(response);
-      console.log(cityQuery.val());
-    })
+
+searchBtn.on("click", function () {
+  var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityQuery.val() + "&appId=60d05752da37a46049341d1d3af701da";
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function(response) {
+    console.log(response);
+    console.log(cityQuery.val()); 
+  });
+});
+
 });
