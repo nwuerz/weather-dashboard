95278daa53aab7f3556d20c1e6898d403bfd70e8
nick wuerz <nawuerz@gmail.com>
2020-02-01 16:58:50 -0600

    used api to style currentCityDiv with weather info

--- script.js
+++ script.js
@@ -14,7 +14,34 @@ searchBtn.on("click", function () {
     method: "GET",
   }).then(function(response) {
     console.log(response);
-    console.log(cityQuery.val()); 
+    var cityName = $("<h5>");
+    cityName.text(response.name);
+    currentCityDiv.append(cityName);
+
+    var todaysDate = $("<p>");
+    //set icon
+    var icon = $("<img>");
+    console.log(response.weather[0].icon);
+    var iconUrl = 'https://openweathermap.org/img/wn/' + response.weather[0].icon + '@2x.png';
+    icon.attr("src", iconUrl);
+    currentCityDiv.append(icon);
+    //set temperature
+    var temp = $("<p>");
+    temp.text( 'Temperature: ' + Math.round(((response.main.temp - 273.15) * 1.8) + 32) + "°F");
+    currentCityDiv.append(temp);
+    // set humidity
+    var humidity = $("<p>");
+    humidity.text( 'Humidity: ' + response.main.humidity);
+    currentCityDiv.append(humidity);
+    //set windspeed
+    var windSpeed = $("<p>");
+    windSpeed.text('Wind Speed: ' + response.wind.speed + ' MPH');
+    currentCityDiv.append(windSpeed);
+    //set UV index
+    var uvIndex = $("<p>");
+    // uvIndex.text('UV index: ' + )
+
+
   });
 });
 
