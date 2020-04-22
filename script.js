27a78c051a8389ae11af138ddce608b00ebe6255
nick wuerz <nawuerz@gmail.com>
2020-04-22 01:38:15 -0500

    fixed forecst dates/data, created function to dry code

--- script.js
+++ script.js
@@ -21,116 +21,25 @@ for (let i = 0; i < citiesArr.length; i++) {
 
 //display weather for an existing city ---------------------//
 cityBtn.on("click", function() {
-  //clear any weather info that is being displayed first
-  currentCityDiv.html(" ");
-  forecastDiv.html(" ");
-  var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + storedCities + owKey;
-  $.ajax({
-    url: queryURL,
-    method: "GET",
-  }).then(function(response) {
-    //city city search to local storage
-    localStorage.setItem('city', storedCities);
-
-    console.log(response);
-    //set city name
-    var cityName = $("<h5>");
-    cityName.text(response.name);
-    currentCityDiv.append(cityName);
-    //set todays date
-    var todaysDate = $("<h6>");
-    todaysDate.text(moment().format("dddd, MMMM Do YYYY"));
-    currentCityDiv.append(todaysDate);
-    //set icon
-    var icon = $("<img>");
-    console.log(response.weather[0].icon);
-    var iconUrl = 'https://openweathermap.org/img/wn/' + response.weather[0].icon + '@2x.png';
-    icon.attr("src", iconUrl);
-    currentCityDiv.append(icon);
-    //set temperature
-    var temp = $("<p>");
-    temp.text( 'Temperature: ' + Math.round(((response.main.temp - 273.15) * 1.8) + 32) + "°F");
-    currentCityDiv.append(temp);
-    // set humidity
-    var humidity = $("<p>");
-    humidity.text( 'Humidity: ' + response.main.humidity);
-    currentCityDiv.append(humidity);
-    //set windspeed
-    var windSpeed = $("<p>");
-    windSpeed.text('Wind Speed: ' + response.wind.speed + ' MPH');
-    currentCityDiv.append(windSpeed);
-    //save country code in var to be used in 5 day forecast
-    var countryCode = response.sys.country;
-    //save city name in var to be used in 5 day forecast-----
-    //make ajax call for uv index
-    var uvUrl =  'https://api.openweathermap.org/data/2.5/uvi/forecast?lat=' + response.coord.lat + '&lon=' + response.coord.lon + owKey;
-    $.ajax({
-      url: uvUrl,
-      method: "GET",
-    }).then(function(response) {
-      //set UV index
-      var uvIndex = $("<p>");
-      uvIndex.text('UV index: ' + response[0].value);
-      currentCityDiv.append(uvIndex);
-      });
-      //make ajax call for 5 day forecast
-      var forecastUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + response.name + ',{' + countryCode + '}'+ owKey;
-      $.ajax({
-        url: forecastUrl,
-        method: "GET",
-      }).then(function(response) {
-        //slice response to obtain only the next 5 days
-        console.log(response.list.slice(0, 5));
-        //store forecast array to a variable
-        var forecastArray = response.list.slice(0, 5);
-        console.log(forecastArray);
-        //create an element for each item that we need to display in the forecast array
-        for (let i = 0; i < forecastArray.length; i++) {
-          //forecast day div
-          var forecastDayDiv = $("<div>");
-          forecastDayDiv.attr("class", "col-sm-2 forecastDayDiv");
-          //date element
-          var todaysDate = moment().add(forecastArray[i]+1, 'days').format('l')
-          var forecastDate = $("<p>");
-          forecastDate.text(todaysDate);
-          forecastDate.attr("class", "forecastDate");
-          forecastDayDiv.append(forecastDate);
-          //icon element
-          var forecastIcon = $("<img>");
-          var forecastIconUrl = 'https://openweathermap.org/img/wn/' + forecastArray[i].weather[0].icon + '@2x.png';
-          forecastIcon.attr("src", forecastIconUrl);
-          forecastDayDiv.append(forecastIcon);
-          //temp element
-          var forecastTemp = $("<p>");
-          forecastTemp.text('Temp: ' + Math.round(((forecastArray[i].main.temp - 273.15) * 1.8) + 32) + "°F");
-          forecastDayDiv.append(forecastTemp);
-          //humidity element
-          var forecastHumidity = $("<p>");
-          forecastHumidity.text('Humidity: ' + forecastArray[i].main.humidity);
-          forecastDayDiv.append(forecastHumidity);
-          //append forcast day div to forecast container
-          forecastDiv.append(forecastDayDiv);
-
-        }
-
-      });
-  });
+  generateWeather(storedCities);
 });
 
 //display weather for a new city--------------------------------//
 searchBtn.on("click", function () {
+  generateWeather(cityQuery.val());
+});
+
+function generateWeather(city){
   //clear any weather info that is being displayed first
   currentCityDiv.html(" ");
   forecastDiv.html(" ");
-  var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityQuery.val() + owKey;
+  var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + owKey;
   $.ajax({
     url: queryURL,
     method: "GET",
   }).then(function(response) {
     //city city search to local storage
-    localStorage.setItem('city', cityQuery.val());
-
-    console.log(response);
+    localStorage.setItem('city', city);
     //set city name
     var cityName = $("<h5>");
     cityName.text(response.name);
@@ -141,7 +50,7 @@ searchBtn.on("click", function () {
     currentCityDiv.append(todaysDate);
     //set icon
     var icon = $("<img>");
-    console.log(response.weather[0].icon);
+    // console.log(response.weather[0].icon);
     var iconUrl = 'https://openweathermap.org/img/wn/' + response.weather[0].icon + '@2x.png';
     icon.attr("src", iconUrl);
     currentCityDiv.append(icon);
@@ -159,7 +68,6 @@ searchBtn.on("click", function () {
     currentCityDiv.append(windSpeed);
     //save country code in var to be used in 5 day forecast
     var countryCode = response.sys.country;
-    //save city name in var to be used in 5 day forecast-----
     //make ajax call for uv index
     var uvUrl =  'https://api.openweathermap.org/data/2.5/uvi/forecast?lat=' + response.coord.lat + '&lon=' + response.coord.lon + owKey;
     $.ajax({
@@ -177,18 +85,15 @@ searchBtn.on("click", function () {
         url: forecastUrl,
         method: "GET",
       }).then(function(response) {
-        //slice response to obtain only the next 5 days
-        console.log(response.list.slice(0, 5));
-        //store forecast array to a variable
-        var forecastArray = response.list.slice(0, 5);
-        console.log(forecastArray);
+        forecastArray = [];
+        forecastArray.push(response.list[10], response.list[18], response.list[26], response.list[34], response.list[39]);
         //create an element for each item that we need to display in the forecast array
         for (let i = 0; i < forecastArray.length; i++) {
           //forecast day div
           var forecastDayDiv = $("<div>");
           forecastDayDiv.attr("class", "col-sm-2 forecastDayDiv");
           //date element
-          var todaysDate = moment().add(forecastArray[i]+1, 'days').format('l')
+          var todaysDate = forecastArray[i].dt_txt.slice(5,10);
           var forecastDate = $("<p>");
           forecastDate.text(todaysDate);
           forecastDate.attr("class", "forecastDate");
@@ -213,6 +118,6 @@ searchBtn.on("click", function () {
 
       });
   });
-});
+}
 
 });
